9e9bd418817ec1b274f8eb1ad10037efed48e0d2
gazzyt <gazzyt@live.co.uk>
2020-05-31 17:42:00 +0100

    Add ADVERTISE_ADDR parameter so that we can run in kubernetes where hostname is not resolvable

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -12,6 +12,11 @@ fi
 echo "ANDAHME Initializing - KAFKA_ZOOKEEPER_CONNECT (${KAFKA_ZOOKEEPER_CONNECT:=zookeeper:2181})"
 sed -i -e "s/^\(zookeeper\.connect=\).*/\1${KAFKA_ZOOKEEPER_CONNECT}/g" ${KAFKA_HOME}/config/server.properties
 
+if [ ${ADVERTISE_ADDR} ]; then
+  echo "Initializing - ADVERTISE_ADDR (${ADVERTISE_ADDR})"
+  sed -i -e "s|#advertised.listeners=PLAINTEXT://your.host.name:9092|advertised.listeners=PLAINTEXT://${ADVERTISE_ADDR}:9092|" ${KAFKA_HOME}/config/server.properties
+fi
+
 
 exec $@
 
